98e45128da2d7520926a620b3f8bec277f7a1303
eltonbrahja <elton.brahja.s@gmail.com>
2026-06-25 16:29:15 +0200

    mod

--- src/pages/CustomBooking.jsx
+++ src/pages/CustomBooking.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {  } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { 
   ArrowLeft, 
   Calendar, 
